905075d22b641cee8d5b9254d601b1c802dac934
Wallysson Cavalcante <wallyssoncavalcante@MacBook-Air-de-Wallysson.local>
2025-09-15 15:06:14 -0300

    First Commit

---
+++ components/navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <nav className="sticky top-0 z-50 bg-white shadow">
+      <div className="container mx-auto flex items-center justify-between p-4">
+        <Link href="/" className="hover:text-blue-600">
+          Meu Ecommerce
+        </Link>
+        <div className="hidden md:flex space-x-6">
+          <Link href="/">Inicio</Link>
+          <Link href="/products" className="hover:text-blue-600">
+            Produtos
+          </Link>
+          <Link href="/checkout" className="hover:text-blue-600">
+            Pagamento
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4"></div>
+      </div>
+    </nav>
+  );
+};
